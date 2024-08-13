1cd21e82f8bd2b15e27c5ca01187ef61a869ff4d
Dustin Lee <dustin.jason.lee@gmail.com>
2024-08-13 12:46:16 -0500

    run application with 1 test data

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,13 @@
 package com.example;
 
+import java.time.LocalDate;
+
+import com.example.controllers.RentalAgreement;
+import com.example.utils.Enums.*;
+
 public class App {
     public static void main(String[] args) {
+        RentalAgreement rentalAgreement = new RentalAgreement(ToolType.JACKHAMMER, 9, LocalDate.of(2015, 7, 2), 0);
+        rentalAgreement.printRentalAgreement();
     }
 }
